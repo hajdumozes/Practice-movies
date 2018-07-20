bbe6f07d0f1913befb55f63761e1e213f86e544a
Mózes <hajdu.mozes@gmail.com>
2018-07-20 15:32:21 +0200

    Task 3 structrure added

--- README.md
+++ README.md
@@ -5,14 +5,14 @@ Top rated movies By IMDB
 * Set all titles to the original titles (probably Swedish is set by default).
 
 ## 2. Filter the list
-* Check, if there are horror films among them. If there are, delete them.
-* Delete films which are older, than me (1994-10-23).
-* Check all task below and above. Delete the keys of every object, you won't need.
+* Check, if there are horror movies among them. If there are, delete them.
+* Delete movies which are older, than me (1994-10-23).
+* Check all task below and above. Delete the keys of every object, you won't need in the future.
 
 ## 3. Tasks with the filtered list
 * Search for all dramas in 1994, which has Tom Hanks, as an actor.
-* Search for the longest and the shortest films among them.
-* Put these two into HTML showing only their title, year, description and poster.
+* Search for the longest and the shortest movies among them.
+* Put these two into HTML showing only their title, year, storyline and poster.
 
 ## 4. Create the Top List Statistics (original list)
 * Get the time interval, when Russel Crowe was a successful actor according to the list.
@@ -24,4 +24,4 @@ Top rated movies By IMDB
 * Put the top 3 year/genre/actor on a medal pedestal in HTML.
 
 ## 5. Search button
-* Search for films by their release date. If there are more, than one, put the highest rated in the one-movie div.
+* Search for movies by their release date. If there are more, than one, put the highest rated in the one-movie div.
--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function setOriginalTitleToTitle(movieListInput) {
 
 // ! 2-1
 
-function removeHorrorFilms(movieListInput) {
+function removeHorrorMovies(movieListInput) {
   var filteredMovieList = [];
   for (var i = 0; i < movieListInput.length; i++) {
     if (!movieListInput[i].genres.includes('Horror')) {
@@ -21,7 +21,126 @@ function removeHorrorFilms(movieListInput) {
   return filteredMovieList;
 }
 
-// todo modifiedMovieList.push(Object.assign({}, movieListInput[i])); Slice helyett
+// ! 2-2
+
+function removeMoviesBefore1994(filteredMovieListInput) {
+  for (var i = 0; i < filteredMovieListInput.length; i++) {
+    if (filteredMovieListInput[i].releaseDate < '1994-10-23') {
+      filteredMovieListInput.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+// ! 2-3
+
+function deleteAdditionalKeys(filteredMovieListInput) {
+  for (var i = 0; i < filteredMovieListInput.length; i++) {
+    delete filteredMovieListInput[i].averageRating;
+    delete filteredMovieListInput[i].contentRating;
+    delete filteredMovieListInput[i].originalTitle;
+    delete filteredMovieListInput[i].posterurl;
+    delete filteredMovieListInput[i].ratings;
+    delete filteredMovieListInput[i].releaseDate;
+  }
+}
+
+// ! 3-1
+
+function searchForJeremyIrons1994DramaMovie(filteredMovieList) {
+  var result = {}; // ? mivel kiderült, hogy csak egy van, nem pusholok.
+  for (var i = 0; i < filteredMovieList.length; i++) {
+    if (filteredMovieList[i].actors.includes('Jeremy Irons') &&
+      filteredMovieList[i].year === '1994' &&
+      filteredMovieList[i].genres.includes('Drama')) {
+      result = (filteredMovieList[i]);
+    }
+  }
+  return result;
+}
+
+// ! 3-2
+
+function searchForLongestAndShortestMovies(filteredMovieList) {
+  var shortest = filteredMovieList[0];
+  var longest = filteredMovieList[0];
+  for (var i = 1; i < filteredMovieList.length; i++) { // ? Duration example: PT142M
+    longest = searchForLongestMovie(filteredMovieList, longest, i);
+    shortest = searchForShortestMovie(filteredMovieList, shortest, i);
+  }
+  return {
+    shortest: shortest,
+    longest: longest
+  };
+}
+
+function searchForLongestMovie(filteredMovieList, longest, i) {
+  if (parseInt(filteredMovieList[i].duration.slice(2), 10) >
+    parseInt(longest.duration.slice(2), 10)) { // ? A parseInt 142M-re is működik
+    return filteredMovieList[i];
+  }
+  return longest;
+}
+
+function searchForShortestMovie(filteredMovieList, shortest, i) {
+  if (parseInt(filteredMovieList[i].duration.slice(2), 10) <
+    parseInt(shortest.duration.slice(2), 10)) {
+    return filteredMovieList[i];
+  }
+  return shortest;
+}
+
+// ! 3-3
+
+function createDivElementMovieStat() {
+  var movieStat = document.createElement('div');
+  movieStat.className = 'movie-stat';
+  return movieStat;
+}
+
+function createDivElementMovieStatTitle() {
+  var movieStatTitle = document.createElement('div');
+  movieStatTitle.className = 'movie-stat-title';
+  return movieStatTitle;
+}
+
+function createDivElementMovieStatStoryline() {
+  var movieStatStoryLine = document.createElement('div');
+  movieStatStoryLine.className = 'movie-stat-storyline';
+  return movieStatStoryLine;
+}
+
+function createDivElementMovieStatPoster() {
+  var movieStatPoster = document.createElement('div');
+  movieStatPoster.className = 'movie-stat-poster';
+  return movieStatPoster;
+}
+
+function insertMovieStatsIntoHTML(chosenStat, selectedDiv) {
+  var movieStat = createDivElementMovieStat();
+  movieStat.appendChild(insertMovieStatTitleAndYear(chosenStat));
+  movieStat.appendChild(insertMovieStatStoryLine(chosenStat));
+  movieStat.appendChild(insertMovieStatPoster(chosenStat));
+  selectedDiv.appendChild(movieStat);
+}
+
+function insertMovieStatTitleAndYear(chosenStat) {
+  var movieStatTitle = createDivElementMovieStatTitle();
+  movieStatTitle.innerHTML = `${chosenStat.title} (${chosenStat.year})`;
+  return movieStatTitle;
+}
+
+function insertMovieStatStoryLine(chosenStat) {
+  var movieStatStoryLine = createDivElementMovieStatStoryline();
+  movieStatStoryLine.innerHTML = `${chosenStat.storyline}`;
+  return movieStatStoryLine;
+}
+
+function insertMovieStatPoster(chosentat) {
+  var movieStatPoster = createDivElementMovieStatPoster();
+  movieStatPoster.innerHTML = `<img src="/img/${chosentat.poster}">`;
+  return movieStatPoster;
+}
 
 function getData(url, callbackFunc) {
   var xhttp = new XMLHttpRequest();
@@ -40,7 +159,17 @@ function successAjax(xhttp) {
   var movieList = JSON.parse(xhttp.responseText);
   // Innen lehet hívni.
   setOriginalTitleToTitle(movieList);
-  var filteredMovieList = removeHorrorFilms(movieList);
-  console.log(filteredMovieList);
+  var filteredMovieList = removeHorrorMovies(movieList);
+  removeMoviesBefore1994(filteredMovieList);
+  deleteAdditionalKeys(filteredMovieList);
+  var movieJeremyIronsDrama1994 = searchForJeremyIrons1994DramaMovie(filteredMovieList);
+  var shortestMovie = searchForLongestAndShortestMovies(filteredMovieList).shortest;
+  console.log(movieJeremyIronsDrama1994);
+  var longestMovie = searchForLongestAndShortestMovies(filteredMovieList).longest;
+  var moviesStats = document.querySelector('.movies-stats');
+  createDivElementMovieStat();
+  insertMovieStatsIntoHTML(movieJeremyIronsDrama1994, moviesStats);
+  insertMovieStatsIntoHTML(shortestMovie, moviesStats);
+  insertMovieStatsIntoHTML(longestMovie, moviesStats);
 }
 getData('/json/top-rated-movies-01.json', successAjax);
